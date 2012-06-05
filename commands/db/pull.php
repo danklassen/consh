debca7c473be640bd83d5faf6ffc76f7c08d6ca8
Dan Klassen <dan@tripleiwebsolutions.com>
2012-06-05 13:24:13 -0700

    db:pull functional
    consh db:pull now imports to local db

--- commands/db/pull.php
+++ commands/db/pull.php
@@ -2,29 +2,46 @@
 
 class DbPull extends Command {
 
-	public function __construct() {
-		$this->name = "DB:Pull";
-		$this->description = "Pull remote database";
-		$this->help = "Pull and import the remote database to the local setup";
-	}
+  public function __construct() {
+    $this->name = "DB:Pull";
+    $this->description = "Pull remote database";
+    $this->help = "Pull and import the remote database to the local setup";
+  }
 
-	public function run($options) {
-		$ssh = new SSH();
-		debug("Pulling remote database");
-		$file_name = 'db_' . time() . '.sql';
-		$remote_file = REMOTE_HOME_PATH.$file_name;
-		debug("remote file: {$remote_file}");
-		$local_file = C5_DIR."{$file_name}";
-		$ssh->runCommand('mysqldump -h ' . REMOTE_DB_HOST . ' -u ' . REMOTE_DB_USER . ' -p'.REMOTE_DB_PASS . ' ' . REMOTE_DB_NAME. " > " . $remote_file);
-		debug('Pulling file locally');
-		$ssh->scp($remote_file, $local_file);
-		$ssh->rmRemoteFile($remote_file);
-		debug('Done');
-		$sql = file_get_contents($local_file);
-		$db = new LocalDB();
-		debug("Importing to local database");
-		$res = $db->execute($sql);
-		debug("Done");
-		return true;
-	}
+  public function run($options) {
+    $ssh = new SSH();
+    debug("Pulling remote database");
+    $file_name = 'db_' . time() . '.sql';
+    $remote_file = REMOTE_HOME_PATH.$file_name;
+    debug("remote file: {$remote_file}");
+    $local_file = C5_DIR."{$file_name}";
+    $ssh->runCommand('mysqldump -h ' . REMOTE_DB_HOST . ' -u ' . REMOTE_DB_USER . ' -p'.REMOTE_DB_PASS . ' ' . REMOTE_DB_NAME. " > " . $remote_file);
+    debug('Pulling file locally');
+    $ssh->scp($remote_file, $local_file);
+    $ssh->rmRemoteFile($remote_file);
+    debug('Done');
+    $sql = file($local_file);
+    $db = new LocalDB();
+    debug("Importing to local database");
+    $templine = '';
+
+    $size = count($sql);
+    $current = 0;
+    foreach ($sql as $line) {
+      $current++;
+      // Skip it if it's a comment
+      if (substr($line, 0, 2) == '--' || $line == '') {
+        continue;
+      }
+      $templine .= $line;
+      if (substr(trim($line), -1, 1) == ';') {
+        $db->execute($templine);
+        showStatus($current, $size);
+        $templine = '';
+      }
+    }
+    print "\n";
+    debug("Done");
+    return true;
+  }
 }
--- includes/functions.php
+++ includes/functions.php
@@ -1,8 +1,11 @@
 <?php
 
-function getInput($msg){
+function getInput($msg, $default = ''){
   fwrite(STDOUT, "$msg: ");
   $varin = trim(fgets(STDIN));
+  if (empty($varin)) {
+  	$varin = $default;
+  }
   return $varin;
 }
 
@@ -56,4 +59,52 @@ function checkConfig($argv = array()) {
 		output("Please run 'consh config' to configure consh");
 		exit;
 	}
+}
+
+/** from http://brian.moonspot.net/status_bar.php.txt
+  */
+function showStatus($done, $total, $size=30) {
+
+    static $start_time;
+
+    // if we go over our bound, just ignore it
+    if($done > $total) return;
+
+    if(empty($start_time)) $start_time=time();
+    $now = time();
+
+    $perc=(double)($done/$total);
+
+    $bar=floor($perc*$size);
+
+    $status_bar="\r[";
+    $status_bar.=str_repeat("=", $bar);
+    if($bar<$size){
+        $status_bar.=">";
+        $status_bar.=str_repeat(" ", $size-$bar);
+    } else {
+        $status_bar.="=";
+    }
+
+    $disp=number_format($perc*100, 0);
+
+    $status_bar.="] $disp%  $done/$total";
+
+    $rate = ($now-$start_time)/$done;
+    $left = $total - $done;
+    $eta = round($rate * $left, 2);
+
+    $elapsed = $now - $start_time;
+
+    $status_bar.= " remaining: ".number_format($eta)." sec.  elapsed: ".number_format($elapsed)." sec.";
+
+    echo "$status_bar  ";
+
+    flush();
+
+    // when done, send a newline
+    if($done == $total) {
+        echo "\n";
+    }
+
 }
