1ca23973361b887648a61fd3c0855a5f8455218e
Dan Klassen <dan@tripleiwebsolutions.com>
2012-06-14 13:01:43 -0700

    comment the code a bit

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * get input from the user. Displays a message and returns the user's input
+ * @param  string $msg message to prompt the user
+ * @param  string $default default value to return if the user does not enter anything
+ * @return string either the user's input or the passed in default if nothing was entered
+ */
 function getInput($msg, $default = ''){
   fwrite(STDOUT, "$msg: ");
   $varin = trim(fgets(STDIN));
@@ -18,11 +24,21 @@ function show_help() {
   }
 }
 
+/**
+ * convert a command like db:pull to the object new DbPull();
+ * @param  string $cmd command to load such as db:pull
+ * @return Object the instantiated object for the command such as DbPull
+ */
 function convertCommandToObject($cmd) {
 	$className = str_replace(' ', '', ucwords(str_replace(':', ' ', $cmd)));
 	return new $className();
 }
 
+/**
+ * convert a classname like DbPull to the full path to the file such as db/pull
+ * @param  string $className name of the class to convert to a path
+ * @return string path of the file relative to the commands directory
+ */
 function convertClassNameToPath($className) {
 	preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $className, $matches);
   $ret = $matches[0];
@@ -32,17 +48,39 @@ function convertClassNameToPath($className) {
   return implode('/', $ret);
 }
 
+/**
+ * function to load classes that haven't been defined yet. This loads from the commands/command_type/command.php
+ * @param  string $className name of the class to load
+ * @return void
+ */
 function __autoload($className) {
 	$path = CONSH_COMMANDS_DIR . convertClassNameToPath($className).".php";
-	require($path);
+  if (file_exists($path)) {
+	 require($path);
+  } else {
+    output('Command not found', 'error');
+    debug("{$path} should define {$className}");
+    die();
+  }
 }
 
+/**
+ * output a message if debugging is enabled
+ * @param  string $msg message to display
+ * @return void
+ */
 function debug($msg) {
 	if(DEBUG) {
-		print $msg."\n";
+		output($msg, 'debug');
 	}
 }
 
+/**
+ * output a message to the console
+ * @param  string $msg  message to display
+ * @param  string $type type of message (ie error, warning, success). Used for color coding the output
+ * @return void
+ */
 function output($msg, $type = '') {
 	if($type != '') {
     $colorCli = new CliColors();
@@ -57,11 +95,16 @@ function output($msg, $type = '') {
 	print $msg."\n";
 }
 
+/**
+ * check to see if the config file exists. Otherwise output a message with instructions to create one
+ * @param  array  $argv array of options from the command line.
+ * @return void
+ */
 function checkConfig($argv = array()) {
 	if(file_exists(CONSH_CONFIG)) {
 		require(CONSH_CONFIG);
 	} else if ((count($argv) < 2) || $argv[1] != 'config') {
-		output("Please run 'consh config' to configure consh");
+		output("Please run 'consh config' to configure consh", 'error');
 		exit;
 	}
 }
