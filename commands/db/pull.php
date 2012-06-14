4a56551c670fe15d21646f30cfc5c3f4acbc13ce
Dan Klassen <dan@tripleiwebsolutions.com>
2012-06-13 21:39:36 -0700

    cleanup after import

--- commands/db/pull.php
+++ commands/db/pull.php
@@ -10,16 +10,16 @@ class DbPull extends Command {
 
   public function run($options) {
     $ssh = new SSH();
-    debug("Pulling remote database");
+    output("Pulling remote database");
     $file_name = 'db_' . time() . '.sql';
     $remote_file = REMOTE_HOME_PATH.$file_name;
     debug("remote file: {$remote_file}");
     $local_file = C5_DIR."{$file_name}";
     $ssh->runCommand('mysqldump -h ' . REMOTE_DB_HOST . ' -u ' . REMOTE_DB_USER . ' -p'.REMOTE_DB_PASS . ' ' . REMOTE_DB_NAME. " > " . $remote_file);
-    debug('Pulling file locally');
+    output('Pulling file locally');
     $ssh->scp($remote_file, $local_file);
     $ssh->rmRemoteFile($remote_file);
-    debug('Done');
+    output('Done', 'success');
     $sql = file($local_file);
     $db = new LocalDB();
     debug("Importing to local database");
@@ -40,8 +40,8 @@ class DbPull extends Command {
         $templine = '';
       }
     }
-    print "\n";
-    debug("Done");
+    unlink($local_file);
+    output("Done", 'success');
     return true;
   }
 }
