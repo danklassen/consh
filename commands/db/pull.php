3eb198e2349409a9b2f811de501318a12c9daf30
Dan Klassen <dan@tripleiwebsolutions.com>
2012-10-22 13:11:30 -0700

    file formatting

--- commands/db/pull.php
+++ commands/db/pull.php
@@ -4,68 +4,75 @@
  *
  * This will overwrite all local data with that from the remote database!
  *
- * @author  Dan Klassen <dan@triplei.ca>
+ * @author    Dan Klassen <dan@triplei.ca>
  * @package Commands
  * @since 0.1
  */
-class DbPull extends Command {
+class DbPull extends Command
+{
 
-  /**
-   * sets the name, description, and help
-   */
-  public function __construct() {
-    $this->name = "DB:Pull";
-    $this->description = "Pull remote database";
-    $this->help = "Pull and import the remote database to the local setup";
-  }
-
-  /**
-   * does the magic
-   *
-   * order of operations:
-   * * create an SSH connection to the remote host
-   * * run mysql dump on remote host
-   * * scp the file locally
-   * * remove the remote file
-   * * import the file to the local database
-   * * remove the local file
-   * @param  array $options not used at all
-   * @return boolean
-   */
-  public function run($options) {
-    $ssh = new SSH();
-    output("Pulling remote database");
-    $file_name = 'db_' . time() . '.sql';
-    $remote_file = REMOTE_HOME_PATH.$file_name;
-    debug("remote file: {$remote_file}");
-    $local_file = C5_DIR."{$file_name}";
-    $ssh->runCommand('mysqldump -h ' . REMOTE_DB_HOST . ' -u ' . REMOTE_DB_USER . ' -p'.REMOTE_DB_PASS . ' ' . REMOTE_DB_NAME. " > " . $remote_file);
-    output('Pulling file locally');
-    $ssh->scp($remote_file, $local_file);
-    $ssh->rmRemoteFile($remote_file);
-    output('Done', 'success');
-    $sql = file($local_file);
-    $db = new LocalDB();
-    debug("Importing to local database");
-    $templine = '';
-
-    $size = count($sql);
-    $current = 0;
-    foreach ($sql as $line) {
-      $current++;
-      // Skip it if it's a comment
-      if (substr($line, 0, 2) == '--' || $line == '') {
-        continue;
-      }
-      $templine .= $line;
-      if (substr(trim($line), -1, 1) == ';') {
-        $db->execute($templine);
-        showStatus($current, $size);
-        $templine = '';
-      }
+    /**
+     * sets the name, description, and help
+     *
+     * @return null
+     */
+    public function __construct()
+    {
+        $this->name = "DB:Pull";
+        $this->description = "Pull remote database";
+        $this->help = "Pull and import the remote database to the local setup";
     }
-    unlink($local_file);
-    output("Done", 'success');
-    return true;
-  }
+
+    /**
+     * does the magic
+     *
+     * order of operations:
+     * * create an SSH connection to the remote host
+     * * run mysql dump on remote host
+     * * scp the file locally
+     * * remove the remote file
+     * * import the file to the local database
+     * * remove the local file
+     *
+     * @param array $options not used at all
+     *
+     * @return boolean
+     */
+    public function run($options = array())
+    {
+        $ssh = new SSH();
+        output("Pulling remote database");
+        $file_name = 'db_' . time() . '.sql';
+        $remote_file = REMOTE_HOME_PATH.$file_name;
+        debug("remote file: {$remote_file}");
+        $local_file = C5_DIR."{$file_name}";
+        $ssh->runCommand('mysqldump -h ' . REMOTE_DB_HOST . ' -u ' . REMOTE_DB_USER . ' -p'.REMOTE_DB_PASS . ' ' . REMOTE_DB_NAME. " > " . $remote_file);
+        output('Pulling file locally');
+        $ssh->scp($remote_file, $local_file);
+        $ssh->rmRemoteFile($remote_file);
+        output('Done', 'success');
+        $sql = file($local_file);
+        $db = new LocalDB();
+        debug("Importing to local database");
+        $templine = '';
+
+        $size = count($sql);
+        $current = 0;
+        foreach ($sql as $line) {
+            $current++;
+            // Skip it if it's a comment
+            if (substr($line, 0, 2) == '--' || $line == '') {
+                continue;
+            }
+            $templine .= $line;
+            if (substr(trim($line), -1, 1) == ';') {
+                $db->execute($templine);
+                showStatus($current, $size);
+                $templine = '';
+            }
+        }
+        unlink($local_file);
+        output("Done", 'success');
+        return true;
+    }
 }
--- commands/files/pull.php
+++ commands/files/pull.php
@@ -7,29 +7,36 @@
  * @package Commands
  * @since 0.1
  */
-class FilesPull extends Command {
+class FilesPull extends Command
+{
 
-	/**
-	 * sets the name, description, and help
-	 */
-	public function __construct() {
-		$this->name = "Files:Pull";
-		$this->description = "Pull remote files locally";
-		$this->help = "Pull remote site's file locally to your /files dir";
-	}
+    /**
+     * sets the name, description, and help
+     *
+     * @return null
+     */
+    public function __construct()
+    {
+        $this->name = "Files:Pull";
+        $this->description = "Pull remote files locally";
+        $this->help = "Pull remote site's file locally to your /files dir";
+    }
 
-	/**
-	 * does the magic
-	 * @param  array $options not used
-	 * @return boolean
-	 */
-	public function run($options) {
-		debug("Pulling remote files");
-		$to_dir = C5_DIR . "/" . "files/";
-		$output = shell_exec('rsync -az --delete '.REMOTE_USER.'@'.REMOTE_HOST.':~/public_html/files/ '. $to_dir);
-		shell_exec('chmod 777 files/ files/cache');
-		debug($output);
-		debug("Done");
-		return true;
-	}
+    /**
+     * does the magic
+     *
+     * @param array $options not used
+     *
+     * @return boolean
+     */
+    public function run($options = array())
+    {
+        debug("Pulling remote files");
+        $to_dir = C5_DIR . "/" . "files/";
+        $output = shell_exec('rsync -az --delete '.REMOTE_USER.'@'.REMOTE_HOST.':~/public_html/files/ '. $to_dir);
+        shell_exec('chmod 777 files/ files/cache');
+        debug($output);
+        debug("Done");
+        return true;
+    }
 }
