bf17dc1899a6c71d9e1e4c27821d3be0ca064e8f
Dan Klassen <dan@tripleiwebsolutions.com>
2012-11-29 21:51:32 -0800

    formatting and hooks on git:push command

--- commands/git/push.php
+++ commands/git/push.php
@@ -3,21 +3,23 @@
  * @author  Dan Klassen <dan@triplei.ca>
  * @package Commands
  */
-class GitPush extends Command {
+class GitPush extends Command 
+{
 
-	public function __construct() {
-		$this->name = "Git:Push";
-		$this->description = "Push local git repo to origin master and pull remote from origin master";
-		$this->help = "Push and pull code";
-	}
+    public function __construct() 
+    {
+        $this->name = "Git:Push";
+        $this->description = "Push local git repo to remote";
+        $this->help = "Push code";
+    }
 
-	public function run($options = array()) {
-		output('pushing to origin master from local');
-		$output = shell_exec('git push origin master');
-		debug($output);
-		output('pulling from origin master from remote');
-		$ssh = new SSH();
-		$output = $ssh->runCommand("cd " . REMOTE_DOC_ROOT . " && git pull origin master");
-		output("Done", 'success');
-	}
+    public function run($options = array()) 
+    {
+        output('pushing to ' .DEPLOY_REMOTE . ' ' . DEPLOY_BRANCH . ' from local');
+        Hook::fire('before_git_push');
+        $output = shell_exec('git push ' . DEPLOY_REMOTE . ' ' . DEPLOY_BRANCH);
+        Hook::fire('after_git_push');
+        debug($output);
+        output("Done", 'success');
+    }
 }
