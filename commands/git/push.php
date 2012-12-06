c321c7fd55a3a3d7c0d72a5fb125006c1e51403a
Dan Klassen <dan@tripleiwebsolutions.com>
2012-12-05 22:06:10 -0800

    remove extra output

--- commands/git/push.php
+++ commands/git/push.php
@@ -3,23 +3,22 @@
  * @author  Dan Klassen <dan@triplei.ca>
  * @package Commands
  */
-class GitPush extends Command 
+class GitPush extends Command
 {
 
-    public function __construct() 
+    public function __construct()
     {
         $this->name = "Git:Push";
         $this->description = "Push local git repo to remote";
         $this->help = "Push code";
     }
 
-    public function run($options = array()) 
+    public function run($options = array())
     {
         output('pushing to ' .DEPLOY_REMOTE . ' ' . DEPLOY_BRANCH . ' from local');
         Hook::fire('before_git_push');
         $output = shell_exec('git push ' . DEPLOY_REMOTE . ' ' . DEPLOY_BRANCH);
         Hook::fire('after_git_push');
-        debug($output);
-        output("Done", 'success');
+        output($output);
     }
 }
