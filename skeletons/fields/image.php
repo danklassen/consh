68fa324d17e0fd46219f23c87b145479dd9afb1c
Dan Klassen <dan@tripleiwebsolutions.com>
2013-02-13 22:01:15 -0800

    make the generated image field work without needing changes

--- skeletons/fields/image.php
+++ skeletons/fields/image.php
@@ -1,6 +1,7 @@
 <div class="control-group">
   <?php print $form->label('{field_name}', t('{field_title}'))?>
   <div class="controls">
-    <?php print $file_form_helper->image('{field_name}', '{field_name}', "Select {field_title}", null);?>
+    <?php ${field_name}_file = File::getByID(${field_name}); ?>
+    <?php print $file_form_helper->image('{field_name}', '{field_name}', "Select {field_title}", ${field_name}_file);?>
   </div>
 </div>
